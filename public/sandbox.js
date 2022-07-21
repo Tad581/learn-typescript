a48f4f0d50935dd7a865f453487ad18eb25474fb
jackk5 <nguyendat58211@gmail.com>
2022-07-21 22:19:24 +0700

    lesson 12: public, private, readonly

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,22 +1,35 @@
 "use strict";
-// let greet: Function;
-// example 1
-var greet;
-greet = function (name, greeting) {
-    console.log("".concat(name, " says ").concat(greeting));
-};
-// example 2
-var calc;
-calc = function (numOne, numTwo, action) {
-    if (action === 'add') {
-        return numOne + numTwo;
+// classes
+var Invoice = /** @class */ (function () {
+    // readonly client: string;
+    // private details: string;
+    // public amount: number;
+    function Invoice(client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
     }
-    else {
-        return numOne - numTwo;
-    }
-};
-// example 3
-var logDetails;
-logDetails = function (ninja) {
-    console.log("".concat(ninja.name, " is ").concat(ninja.age, " years old"));
-};
+    Invoice.prototype.format = function () {
+        return "".concat(this.client, " owes \u00A3").concat(this.amount, " for ").concat(this.details);
+    };
+    return Invoice;
+}());
+var invOne = new Invoice('mario', 'work on the mario website', 250);
+var invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+var invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+invoices.forEach(function (inv) {
+    console.log(inv.client, /*inv.details,*/ inv.amount, inv.format());
+});
+var form = document.querySelector('.new-item-form');
+console.log(form.children);
+// inputs
+var type = document.querySelector('#type');
+var tofrom = document.querySelector('#tofrom');
+var details = document.querySelector('#details');
+var amount = document.querySelector('#amount');
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+});
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,10 +1,33 @@
-const anchor = document.querySelector('a')!;
-if(anchor) {
-  console.log(anchor.href);
-}
-console.log(anchor.href);
+// classes
+class Invoice {
+  // readonly client: string;
+  // private details: string;
+  // public amount: number;
 
-//const form = document.querySelector('form')!;
+  constructor(
+    readonly client: string, 
+    private details: string, 
+    public amount: number,
+  ){}
+
+  format() {
+    return `${this.client} owes £${this.amount} for ${this.details}`;
+  }
+}
+
+const invOne = new Invoice('mario', 'work on the mario website', 250);
+const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+
+let invoices: Invoice[] = [];
+invoices.push(invOne)
+invoices.push(invTwo);
+
+invoices.forEach(inv => {
+  console.log(inv.client, /*inv.details,*/ inv.amount, inv.format());
+})
+
+
+
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 console.log(form.children);
 
