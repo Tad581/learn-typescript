3858184e6d7c8d4acf48f4ca8b14e5834253f210
jackk5 <nguyendat58211@gmail.com>
2022-07-21 21:44:04 +0700

    lesson 10: DOM & type casting

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,26 +1,26 @@
-// let greet: Function;
-
-// example 1
-let greet: (a: string, b: string) => void;
-
-greet = (name: string, greeting: string) => {
-  console.log(`${name} says ${greeting}`);
+const anchor = document.querySelector('a')!;
+if(anchor) {
+  console.log(anchor.href);
 }
+console.log(anchor.href);
 
-// example 2
-let calc: (a: number, b: number, c: string) => number;
+//const form = document.querySelector('form')!;
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+console.log(form.children);
 
-calc = (numOne: number, numTwo: number, action: string) => {
-  if (action === 'add') {
-    return numOne + numTwo;
-  } else {
-    return numOne - numTwo;
-  }
-}
+// inputs
+const type = document.querySelector('#type') as HTMLInputElement;
+const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
 
-// example 3
-let logDetails: (obj: {name: string, age: number}) => void;
+form.addEventListener('submit', (e: Event) => {
+  e.preventDefault();
 
-logDetails = (ninja: {name: string, age: number}) => {
-  console.log(`${ninja.name} is ${ninja.age} years old`);
-}
+  console.log(
+    type.value, 
+    tofrom.value, 
+    details.value, 
+    amount.valueAsNumber
+  );
+});
